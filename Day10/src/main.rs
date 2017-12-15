1c1b119009919526e0a7dbc0177de472f8a38cb3
Matthijs Hofstra <thiezz@gmail.com>
2017-12-15 17:12:17 +0100

    Answer for day 10, using the knot_hash library

--- Day10/src/main.rs
+++ Day10/src/main.rs
@@ -1,3 +1,19 @@
+extern crate knot_hash;
+extern crate load_input;
+
 fn main() {
-    println!("Hello, world!");
+    let input = load_input::load_input();
+    let first_input = input
+        .split(',')
+        .map(str::trim)
+        .flat_map(str::parse);
+    
+    let ks = knot_hash::KnotState::new()
+        .perform_round(first_input);
+    let first_hash = ks.list[0] as u16 * ks.list[1] as u16;
+    
+    let second_hash = knot_hash::knot_hash(input.as_bytes());
+
+    println!("First answer: {}", first_hash);
+    println!("Second answer: {}", &*second_hash);
 }
